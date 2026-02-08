80aece69c86efd20e5a03321c1ba0bd65e547313
Raiytak <mathieu.salaun12@gmail.com>
2026-02-08 16:05:46 -0500

    codecrafters submit [skip ci]

--- app/command/echo.go
+++ app/command/echo.go
@@ -6,6 +6,15 @@ import (
 )
 
 func EchoCmd(args []string) {
+  firstChar, lastChar := args[0][0], args[len(args)][len(len(args))]
+  if firstChar == "'" || firstChar == "\"" {
+    if firstChar != lastChar {
+      fmt.Print("Unclosed string\n")
+      return
+    }
+    args[0] = args[0][1:]
+    args[len(args)] = args[len(args)][:len(args[len(args])]
+  }
 	fmt.Println(strings.Join(args, " "))
 
 }
