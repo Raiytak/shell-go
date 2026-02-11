f82c73353e101c49796ef26e4305484fb191fda2
Raiytak <mathieu.salaun12@gmail.com>
2026-02-11 09:21:59 -0500

    codecrafters submit [skip ci]

--- app/command/pwd.go
+++ app/command/pwd.go
@@ -7,9 +7,9 @@ import (
 func PwdCmd(s Shell, args []string) {
 	var lines []string
 	if len(args) != 0 {
-		lines = []string{("pwd: too many arguments\n")}
+		lines = []string{("pwd: too many arguments")}
 	} else {
-		lines = []string{fmt.Sprintf("%s\n", s.WorkingDir())}
+		lines = []string{s.WorkingDir()}
 	}
 	display(s, lines)
 }
